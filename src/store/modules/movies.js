35bd3bb812dd2268aadbf989f85390196625b0be
Amiran <chicobes1@gmail.com>
2022-05-05 18:06:16 +0300

    completed the setup store

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,27 +1,60 @@
 import axios from '@/plugins/axios';
+import IDs from '@/store/mock/imdb_top250';
+import mutations from '@/store/mutations';
+
+
+function serializeResponse(movies)
+{
+	return movies.reduce((acc, movie) =>
+	{
+		acc[movie.imdbID] = movie;
+		return acc;
+	}, {});
+}
+
+const { MOVIES } = mutations;
 
 const moviesStore =
 {
 	namespaced: true,
-	state:{},
-	mutations:{},
-	actions:
+	state:
 	{
-		async fetchMovies(context)
+		top250IDs: IDs,
+		currentPage: 1,
+		moviesPerPage: 12,
+		movies: {},
+	},
+	mutations:
+	{
+		[MOVIES](state, value)
 		{
-			console.log(context);
-			//tt0111161
-			const response = await axios.get('/', {
-				params: {
-					apikey: process.env.VUE_APP_API_KEY,
-					plot: 'full',
-					i: 'tt0111161'
-				}
-			});
-			console.log(response);
+			state.movies = value;
 		}
 	},
-	getters:{},
+	actions:
+	{
+		async fetchMovies({ getters, commit })
+		{
+			try
+			{
+				const { currentPage, moviesPerPage, sliceedIds } = getters;
+				const from = currentPage * moviesPerPage - moviesPerPage;
+				const to = currentPage * moviesPerPage;
+				const moviesToFetch = sliceedIds(from, to);
+				const request = moviesToFetch.map(id => axios.get(`http://www.omdbapi.com/?i=${id}`))
+				const response = await Promise.all(request);
+				const movies = serializeResponse(response);
+				commit(MOVIES, movies);
+			}
+			catch (err) { console.log(err) };
+		}
+	},
+	getters:
+	{
+		sliceedIds: ({ top250IDs }) => (from, to) => top250IDs.slice(from, to),
+		currentPage: ({ currentPage }) => currentPage,
+		moviesPerPage: ({ moviesPerPage }) => moviesPerPage,
+	},
 };
 
 export default moviesStore;
